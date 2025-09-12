7130ae20ae0033fc7bfffd3726f6fac08ea5ea64
Prashann raj <prashannraj@hotmail.com>
2025-09-12 17:58:19 +0545

    duplicate route name fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,7 @@
               <div class="text-center text-muted mb-4">
                 <small>Sign in with credentials</small>
               </div>
-              <form method="POST" action="{{ route('login') }}">
+              <form method="POST" action="{{ route('login.attempt') }}">
                 @csrf
                 <div class="form-group mb-3">                                
                   <div class="input-group input-group-merge input-group-alternative">
