109dd0b807206aa591c68f60dbc5bd9608b1a60d
Prashann raj <prashannraj@hotmail.com>
2025-09-15 16:44:39 +0545

    config mail edited

--- resources/views/auth/emails/otp.blade.php
+++ resources/views/auth/emails/otp.blade.php
@@ -1,31 +1,50 @@
+<!-- resources/views/auth/emails/otp.blade.php -->
+
 <p>Dear user,</p>
 
 <p>Verification needed.</p>
-<p>Please confirm your sign-in request.
-</p>
-<p>We have detected an account sign-in request from a device we don't recognise.
-</p>
+<p>Please confirm your sign-in request.</p>
+<p>We have detected an account sign-in request from a device we don't recognise.</p>
+
 <ul>
     <li>
-        Account: <b>{{$data['email']}}</b>
-
+        Account: <b>{{ $data['email'] ?? 'N/A' }}</b>
     </li>
-    <li>When: <b>{{now()}}</b></li>
-    <!-- <li>Device: <b>{{$data['device']}}</b>
+    <li>
+        When: <b>{{ now() }}</b>
     </li>
-    <li>Location: <b>{{$data['location']}}</b>
-    </li> -->
+    @if(isset($data['device']))
+        <li>
+            Device: <b>{{ $data['device'] }}</b>
+        </li>
+    @endif
+    @if(isset($data['location']))
+        <li>
+            Location: <b>{{ $data['location'] }}</b>
+        </li>
+    @endif
 </ul>
-Location is aproximate based on the login's IP address
 
-<p>To verify your account is safe, please use the following code to enable your new device — it will expire in 15 minutes or when you refresh your browser:
-</p>
-<p>OTP Code: <b>{{$data['otp']}}</b>
-</p><br>
-<p>That wasn't me! Report to the company manager or IT professional.
-</p>
-<p>Thank you.</p>
+<p>Location is approximate based on the login's IP address.</p>
+
 <p>
-    {{$data['companyinfo']->name}}<br>
-    {{$data['companyinfo']->address}}<br>
+    To verify your account is safe, please use the following code to enable your new device — it will expire in 15 minutes or when you refresh your browser:
 </p>
+
+<p>
+    OTP Code: <b>{{ $data['otp'] ?? 'N/A' }}</b>
+</p>
+
+<br>
+
+<p>
+    That wasn't me! Report to the company manager or IT professional.
+</p>
+
+<p>Thank you.</p>
+
+<p>
+    {{ optional($data['companyinfo'])->name ?? 'Company Name' }}<br>
+    {{ optional($data['companyinfo'])->address ?? 'Company Address' }}<br>
+</p>
+    
